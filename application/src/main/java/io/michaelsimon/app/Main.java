c3866876d11a9c49b3681d933ee4ad5b310977c1
Michael <mail@michaelsimon.io>
2017-01-28 10:41:53 +0100

    added spring boot componentscan for integration be services

--- application/src/main/java/io/michaelsimon/app/Main.java
+++ application/src/main/java/io/michaelsimon/app/Main.java
@@ -2,8 +2,10 @@ package io.michaelsimon.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "io.michaelsimon.app")
 public class Main {
 
     public static void main(String[] args) {
