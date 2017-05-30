2705165e75ba14acb90c5fc1ee6a4dbba1286de1
Michael <mail@michaelsimon.io>
2017-05-30 19:24:44 +0200

    commit to get back in a commit-order

--- backend/src/test/java/io/michaelsimon/app/backend/service/DummyServiceTest.java
+++ backend/src/test/java/io/michaelsimon/app/backend/service/DummyServiceTest.java
@@ -1,5 +1,13 @@
 package io.michaelsimon.app.backend.service;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class DummyServiceTest {
+
+    @Test
+    public void dummy() {
+        Assert.assertTrue(true);
+    }
 
 }
